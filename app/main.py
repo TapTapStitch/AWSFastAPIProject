a817030847218ce21638af5a5fda590cff439f6f
Mykyta Zub <Mykyta_Zub@epam.com>
2025-01-06 20:42:28 +0200

    added postman collection

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from mangum import Mangum
 from app.posts.router import router as posts_router
 
 app = FastAPI()
-handler = Mangum(app)
 
 app.include_router(posts_router, tags=["posts"])
 
@@ -11,3 +10,6 @@ app.include_router(posts_router, tags=["posts"])
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the FAST API running with AWS Lambda"}
+
+
+handler = Mangum(app)
